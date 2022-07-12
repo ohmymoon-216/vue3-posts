71e9fa2683d6f67a7949d2f3281acdfb307195dd
DESKTOP-DS9GTQL\ohmym <ohmymoon@gmail.com>
2022-07-12 17:07:50 +0900

    first commit

---
+++ src/layouts/TheHeader.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <header>
+      <nav class="navbar navbar-expand-sm navbar-dark bg-primary">
+        <a class="navbar-brand" href="#">GYM CODING</a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto">
+            <li class="nav-item">
+              <a class="nav-link active" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">About</a>
+            </li>
+          </ul>
+          <form class="d-flex">
+            <input
+              class="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">
+              Search
+            </button>
+          </form>
+        </div>
+      </nav>
+    </header>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
---
+++ src/layouts/TheView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <main>
+      <div class="container py-4">
+        <h2>Hello Vue3</h2>
+      </div>
+    </main>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
